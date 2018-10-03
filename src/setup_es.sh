a5c91fbc021dce881394a4484dc42dc0586c8e2a
tohud <ss16th@yahoo.co.jp>
2018-10-03 23:42:26 +0900

    コメント追記

--- src/setup_es.sh
+++ src/setup_es.sh
@@ -6,7 +6,7 @@ curl -XDELETE 'localhost:9200/gacrp_index'
 echo "gacrp_indexを再作成"
 curl -XPUT -H 'Content-type:application/json' 'localhost:9200/gacrp_index' -d @mapping_gacrp.json
 
-# 
+# ESのbulk insertは20MBぐらいのファイル推奨のため、ファイルを分割してから投げる
 echo "bulk insertファイル作成"
 split --lines 20000 ${GACRP_HOME}/dat/train_blk.json train_blk_split_
 sp="train_blk_split_aa"
